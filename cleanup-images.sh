a9206e386852b1534e540f3cc755b80409cee4d4
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-09-27 13:08:59 +0200

    Capture images using created date

--- cleanup-images.sh
+++ cleanup-images.sh
@@ -9,7 +9,7 @@ fi
 
 echo "Cleaning up untagged images..."
 
-IMAGE_IDS=($(docker images | grep "<none>" | grep "$WHEN" | awk '{print $3}'))
+IMAGE_IDS=($(docker images | grep "<none>" | awk '{print $3,$4,$5}' | grep "$WHEN" | awk '{print $1}'))
 
 for IMAGE_ID in "${IMAGE_IDS[@]}"
 do
