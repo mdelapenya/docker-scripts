12a650fcd726a2d5b0e364597dc6b2ade1975a73
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-09-27 09:47:20 +0200

    Add script to delete untagged images

---
+++ cleanup-images.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+WHEN=${1}
+
+if [[ -z "$WHEN" ]]
+then
+        WHEN="week"
+fi
+
+echo "Cleaning up untagged images..."
+
+IMAGE_IDS=($(docker images | grep "<none>" | grep "$WHEN" | awk '{print $1}'))
+
+for IMAGE_ID in "${IMAGE_IDS[@]}"
+do
+        echo "Removing image with imageId: $IMAGE_ID"
+        docker rmi $IMAGE_ID
+done
