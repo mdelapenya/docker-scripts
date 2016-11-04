00ac334504195a106377ba7d30f307e66b399c20
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-11-04 13:47:08 +0100

    Invoke containers cleanup with parameters

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DEBUG=false
+EXCLUDED="registry"
 IMAGE=""
 WHEN="2 weeks"
 
@@ -10,6 +11,7 @@ usage() {
 	echo "	[-h|--help]: Prints this usage message"
 	echo "	[-i|--image IMAGE NAME]: Define which image names and tags should be deleted"
 	echo "	[-w|--when PATTERN]: Define the time range to delete the images. If no value is set '2 weeks' will be used"
+	echo "	[-x|--excluded CONTAINER NAME]: Define which container name should be excluded from deletion. By default, registry will be never deleted"
 }
 
 validate_image() {
@@ -43,6 +45,11 @@ do
 
 			shift 2
 			;;
+		-x | --excluded)
+			EXCLUDED="$2"
+
+			shift 2
+			;;
 		-* | --*) # End of all options
 			usage
 			exit 1
@@ -55,13 +62,15 @@ done
 
 validate_image
 
-./cleanup-containers.sh $WHEN
-
 if [ "$DEBUG" = true ]; then
+  ./cleanup-containers.sh -d -w $WHEN -x $EXCLUDED
+
   ./cleanup-images.sh -d -i $IMAGE -w "$WHEN"
 
   ./cleanup-orphan-images.sh -d
 else
+  ./cleanup-containers.sh -w $WHEN -x $EXCLUDED
+
   ./cleanup-images.sh -i $IMAGE -w "$WHEN"
 
   ./cleanup-orphan-images.sh
